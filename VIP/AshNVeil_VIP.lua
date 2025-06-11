d1fdbfef85fe74d6bf5000a102a60ef36b9cbdd8
Aiueo089 <132821646+Noyz028@users.noreply.github.com>
2025-06-11 18:22:05 +0800

    Add files via upload

---
+++ VIP/AshNVeil_VIP.lua
@@ -0,0 +1,340 @@
+
+
+
+target = gg.getTargetInfo()
+
+if (not target) then
+    gg.alert("Failed to get target info.")
+    do return end
+end
+
+if (not target.x64) then
+    gg.alert("This script is not compatible with 32-bit.")
+    do return end
+end
+
+--███████████████████████
+Package = "studio.gameberry.anv"
+appName = "Ash N Veil"
+packageName = gg.getTargetPackage()
+v=gg.getTargetInfo()
+if v.processName~=Package then
+        gg.alert("========================\nScript by ⊹иσιcε⊹\nYoutube = ⊹иσιcε⊹\n========================\n       ✧ This Script For ✧\nGame = "..appName.."\n========================\n       ✧ You Selected ✧\nGame = "..v.label.."\nVersion = ["..v.versionName.."]\n========================")
+    os.exit()
+end
+--███████████████████████
+
+function main()
+     menu = gg.choice({
+        "🔸Free Shop🔸",
+        "EXIT"},
+        0, 
+        "✧ Ash N Veil VIP ✧\n✧ YouTube ⊹иσιcε⊹ ✧")
+
+if menu == 1 then
+FreeShop()
+end
+ 
+if menu == 2 then
+exit = string.format("======================\nScript by ⊹иσιcε⊹\n\nYoutube = ⊹иσιcε⊹\n\nGame = %s\n\nVersion = [%s]\n======================",  appName,  v.versionName)
+  print(exit)
+gg.clearResults()
+  gg.clearList()
+  gg.setVisible(true)
+  os.exit()
+end
+end 
+
+--============================
+function FreeShop()
+gg.clearResults()
+gg.clearList()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("hA0BB0D00FFFFFFFF00000000", 1)
+gg.refineNumber("-96", 1)
+t=4 o=0x4 offset()
+gg.refineNumber("-1", 4)
+t=4 o=0x18 offs() offset() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+t=4 o=0x50 offset() mlop() edit()
+
+gg.clearResults()
+
+gg.toast("Succes")
+end
+--█████████████████████████████████████
+stop = false
+function mlop()
+gg.setVisible(false)
+    if stop then return end
+    local awal = gg.getResults(3)
+    gg.loadResults({awal[3]})
+    gg.refineNumber("0~500000", t)
+    if gg.getResultsCount() == 0 then
+        stop = true
+        gg.clearResults()
+        return
+    end
+    gg.loadResults(awal)
+end
+------------------------------------------------------------------------------  
+function offs()
+gg.setVisible(false)
+    if stop then return end
+adio = {}
+ad = gg.getResults(gg.getResultsCount())
+for i, v in ipairs(ad) do
+    table.insert(adio, {address = v.address, flags = t})
+    table.insert(adio, {address = v.address + 4, flags = t})
+    table.insert(adio, {address = v.address + 8, flags = t})
+end
+gg.loadResults(adio)
+end
+
+function offset()
+gg.setVisible(false)
+    if stop then return end
+o=tonumber(o) addoff=nil 
+addoff=gg.getResults(gg.getResultsCount())
+    for i, v in ipairs(addoff) do
+        addoff[i].address=addoff[i].address+o
+        addoff[i].flags=t
+    end
+gg.loadResults(addoff) 
+end
+------------------------------------------------------------------------------  
+count = 0
+totall = 200
+function edit()
+gg.setVisible(false)
+    if stop then return end
+count = count + 1
+kkk = gg.getResults(gg.getResultsCount())
+gg.addListItems(kkk)
+kkk = gg.getListItems()
+for i, v in ipairs(kkk) do
+    if i == 1 then
+        v.value = "1"
+    elseif i == 2 then
+        v.value = "2001"
+    elseif i == 3 then
+        v.value = "1"
+    end
+    if i <= 3 and v.flags == t then
+        v.freeze = true
+        v.freezeType = gg.FREEZE_NORMAL
+    end
+end
+
+gg.addListItems(kkk)
+gg.clearList()
+gg.toast(tostring(math.floor(count*100/totall)).."%")
+end
+--█████████████████████████████████████
+--============================
+--============================
+--█████████████████████████████████████
+
+while true do
+if gg.isVisible(true) then
+gg.setVisible(false)
+main()
+end
+end
